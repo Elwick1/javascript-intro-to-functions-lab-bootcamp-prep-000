06ad3804c7fede4aa0619a5c77e23a171e19a65d
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-16 00:04:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
- if (string) string.toLowerCase() === string
- if (string) string.toUpperCase() === string
-  }
-  
+ 	if (string.toLowerCase() === string) {
+      
+ 	} else if (string.toUpperCase() === string) {
+      
+    }
+}
 
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
