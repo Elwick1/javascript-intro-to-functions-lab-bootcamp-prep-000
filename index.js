b62bf39195a56084936382b059b2922be505a0a9
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-16 00:10:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,19 +21,6 @@ function sayHiToGrandma(string) {
  	} else if (string.toUpperCase() === string) {
       
     }
-    return string.toUpperCase()
+    return 'I can't hear you!()
 }
 
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
